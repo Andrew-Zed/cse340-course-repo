15191f5c8a3c34388ce91dcc47a5f139dc312d10
Andrew Awadike <andrew.awadyke@gmail.com>
2026-05-16 04:47:14 +0100

    Added db connection ansd rendered log and organisation from the db

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 const NODE_ENV = process.env.NODE_ENV?.toLocaleLowerCase() || "production";
 const PORT = process.env.PORT || 3000;
@@ -23,8 +25,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+
     const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+    res.render('organizations', { title, organizations });
 })
 
 app.get('/projects', (req, res) => {
@@ -37,7 +41,15 @@ app.get('/categories', (req, res) => {
     res.render('categories', { title });
 })
 
-app.listen(PORT, () => {
-console.log(`Server is running at http://127.0.0.1:${PORT}`);
-console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`); 
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+        process.exit(); // Exit with a failure code
+    }
+
 })
